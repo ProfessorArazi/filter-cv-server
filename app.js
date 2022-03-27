fee22ff2b2468f0f1a91f5b6765e823f61809951
Amit <amitarazi12345@gmail.com>
2022-03-27 12:29:25 +0300

    added vercel

--- app.js
+++ app.js
@@ -85,3 +85,5 @@ app.post("/upload/:keys/:email/:option", (req, res) => {
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+module.exports = app;
